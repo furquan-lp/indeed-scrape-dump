aa89355d21636db6b2eaa11739ec60d3a4fc187b
Syed F Ahmad <furquan@protonmail.com>
2024-01-15 02:11:00 +0530

    get_capital_keyword_jobs: Added implementation

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from dotenv import load_dotenv
 from typing import Final
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.database import Database
 
 load_dotenv()
 
 MONGO_DB_URI: Final[str] = environ['DUMP_DB_URI']
+MONGO_DB_NAME: Final[str] = environ['DUMP_DB_NAME']
 
+keywords: list[str] = ["software", "data scientist", "java", "python", "devops", "web developer", "backend", "cloud",
+                       "junior developer", "senior developer"]
 client: MongoClient = MongoClient(MONGO_DB_URI, server_api=ServerApi('1'))
 app = FastAPI()
 
@@ -19,6 +23,8 @@ try:
     print('Successfully connected to MongoDB.')
 except Exception as e:
     print(f'Couldn\'t connect to the database: {e}', file=stderr)
+
+db: Database = client[MONGO_DB_NAME]
 
 
 @app.get('/')
@@ -33,14 +39,25 @@ async def root() -> HTMLResponse:
     return HTMLResponse(content=index_html)
 
 
-@app.get('/jobs/capitals/{city}')
+@app.get('/jobs/capitals/city/{city}')
 async def get_capital_city_jobs(city: str):
     pass
 
 
 @app.get('/jobs/capitals/{keyword}')
 async def get_capital_keyword_jobs(keyword: str):
-    pass
+    if keyword not in keywords:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail='Keyword not valid for the database')
+    else:
+        collection = db[keyword]
+        items = collection.find()
+        found_items: list[dict] = []
+        for item in items:
+            current_item = {'city': item.get('city', ''), 'state': item.get(
+                'state', ''), 'jobs': item.get('jobs', {})}
+            found_items.append(current_item)
+        return {keyword: found_items}
 
 
 @app.get('/jobs/capitals/{keyword}/{city}')
